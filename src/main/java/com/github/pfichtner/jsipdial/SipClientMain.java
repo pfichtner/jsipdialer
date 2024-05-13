6e75a3b3dfd3afcd45f5d8cf56dc6c3599985d0d
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-05-13 23:38:16 +0200

    read config values via command line arguments

--- src/main/java/com/github/pfichtner/jsipdial/SipClientMain.java
+++ src/main/java/com/github/pfichtner/jsipdial/SipClientMain.java
@@ -2,37 +2,59 @@ package com.github.pfichtner.jsipdial;
 
 import static java.lang.Integer.parseInt;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.Properties;
+import org.apache.commons.cli.DefaultParser;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
 
 import com.github.pfichtner.jsipdial.messages.MessageFactory;
 
 public class SipClientMain {
 
+	private static final String TIMEOUT = "timeout";
+	private static final String CALLER_NAME = "callerName";
+	private static final String DESTINATION_NUMBER = "destinationNumber";
+
+	private static final String PASSWORD = "sipPassword";
+	private static final String USERNAME = "sipUsername";
+
+	private static final String SIP_SERVER_PORT = "sipServerPort";
+	private static final String SIP_SERVER_ADDRESS = "sipServerAddress";
+
 	public static void main(String[] args) throws Exception {
-		Properties properties = loadProperties("SipClientMain.config");
+		var options = options();
+		var parser = new DefaultParser();
 
-		var sipServerAddress = properties.getProperty("sipServerAddress");
-		var sipServerPort = parseInt(properties.getProperty("sipServerPort"));
-		var username = properties.getProperty("username");
-		var password = properties.getProperty("password");
-
-		var destinationNumber = properties.getProperty("destinationNumber");
-		var callerName = properties.getProperty("callerName");
-		var timeout = parseInt(properties.getProperty("timeout"));
-
-		try (var connection = new Connection(sipServerAddress, sipServerPort, username, password)) {
-			new CallExecutor(connection, new MessageFactory())
-					.execCall(new Call(destinationNumber, callerName, timeout));
+		try {
+			var cmdLine = parser.parse(options, args);
+			try (var connection = new Connection( //
+					cmdLine.getOptionValue(SIP_SERVER_ADDRESS), //
+					parseInt(cmdLine.getOptionValue(SIP_SERVER_PORT, "5060")), //
+					cmdLine.getOptionValue(USERNAME, System.getenv("SIP_USERNAME")), //
+					cmdLine.getOptionValue(PASSWORD, System.getenv("SIP_PASSWORD")) //
+			)) {
+				new CallExecutor(connection, new MessageFactory()).execCall(new Call( //
+						cmdLine.getOptionValue(DESTINATION_NUMBER), //
+						cmdLine.getOptionValue(CALLER_NAME), //
+						parseInt(cmdLine.getOptionValue(TIMEOUT, "15")) //
+				));
+			}
+		} catch (ParseException e) {
+			e.printStackTrace();
+			new HelpFormatter().printHelp("args...", options);
 		}
+
 	}
 
-	private static Properties loadProperties(String filename) throws IOException, FileNotFoundException {
-		Properties properties = new Properties();
-		properties.load(new FileInputStream(filename));
-		return properties;
+	private static Options options() {
+		return new Options() //
+				.addOption(SIP_SERVER_ADDRESS, true, "") //
+				.addOption(SIP_SERVER_PORT, true, "") //
+				.addOption(USERNAME, true, "") //
+				.addOption(PASSWORD, true, "") //
+				.addOption(DESTINATION_NUMBER, true, "") //
+				.addOption(CALLER_NAME, true, "") //
+				.addOption(TIMEOUT, true, "");
 	}
 
 }
