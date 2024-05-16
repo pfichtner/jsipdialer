73d8fcfee407d4d1ac84639a098a449e4dc51c78
Peter Fichtner (pfichtner) <pfichtner@users.noreply.github.com>
2024-05-16 21:20:42 +0200

    house keeping

--- src/test/java/com/github/pfichtner/jsipdialer/CallExecutorTest.java
+++ src/test/java/com/github/pfichtner/jsipdialer/CallExecutorTest.java
@@ -3,6 +3,7 @@ package com.github.pfichtner.jsipdialer;
 import static com.github.pfichtner.jsipdialer.messages.SipStatus.UNAUTHORIZED;
 import static java.util.Collections.emptyList;
 import static java.util.concurrent.Executors.newSingleThreadExecutor;
+import static java.util.concurrent.TimeUnit.SECONDS;
 import static java.util.function.Predicate.isEqual;
 import static org.awaitility.Awaitility.await;
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -11,6 +12,8 @@ import static org.junit.jupiter.api.Assertions.assertNull;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
+import java.util.function.Function;
+import java.util.function.Predicate;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Timeout;
@@ -20,10 +23,10 @@ import com.github.pfichtner.jsipdialer.messages.MessageReceived;
 import com.github.pfichtner.jsipdialer.messages.MessageToSend;
 import com.github.pfichtner.jsipdialer.messages.Statuscode;
 
-@Timeout(CallExecutorTest.TIMEOUT)
+@Timeout(value = CallExecutorTest.TIMEOUT_SECONDS, unit = SECONDS)
 class CallExecutorTest {
 
-	static final int TIMEOUT = 60;
+	static final int TIMEOUT_SECONDS = 60;
 
 	@Test
 	void noResponseWillTerminateAfterTimeout() throws Exception {
@@ -43,29 +46,35 @@ class CallExecutorTest {
 
 	@Test
 	void sendsAuthOnUnauthorizedResponse() throws Exception {
+		String realm = "XXX";
+		String nonce = "YYY";
 		ConnectionStub connection = new ConnectionStub() {
 			@Override
 			public MessageReceived receive() throws IOException {
 				var status = UNAUTHORIZED;
-				var data = Map.of("WWW-Authenticate", "realm=\"XXX\", nonce=\"YYY\"");
-				return new MessageReceived("SIP/2.0 ", new Statuscode(status.value()), status.name(), data, emptyList());
+				var data = Map.of("WWW-Authenticate", "realm=\"" + realm + "\", nonce=\"" + nonce + "\"");
+				return new MessageReceived("SIP/2.0 ", new Statuscode(status.value()), status.name(), data,
+						emptyList());
 			}
 		};
 		CallExecutor callExecutor = new CallExecutor(connection, new SipConfig("aSipUser", "secret"),
 				new MessageFactory());
-		Call call = new Call("123", "theCallersName", 2 * TIMEOUT);
+		Call call = new Call("123", "theCallersName", 2 * TIMEOUT_SECONDS);
 
 		callInBackground(callExecutor, call);
 		String key = "Authorization";
-		String expectedValue = "Digest " //
-				+ "username=\"aSipUser\", " //
-				+ "realm=\"XXX\", " //
-				+ "nonce=\"YYY\", " //
-				+ "uri=\"sip:123@" + connection.remoteServerAddress() + "\", " //
-				+ "response=\"d7279a9da44929e24abb213421b33f96\", " //
-				+ "algorithm=\"MD5\"" //
-		;
-		await().until(() -> connection.sent().stream().anyMatch(m -> expectedValue.equals(m.lines().get(key))));
+		String expectedValue = """
+				Digest \
+				username="aSipUser", \
+				realm="%s", \
+				nonce="%s", \
+				uri="sip:%s@%s", \
+				response="d7279a9da44929e24abb213421b33f96", \
+				algorithm="MD5"\
+				""".formatted(realm, nonce, call.destinationNumber, connection.remoteServerAddress());
+		await().forever()
+				.until(() -> connection.sent().stream().filter(where(MessageToSend::command, isEqual("INVITE")))
+						.anyMatch(m -> expectedValue.equals(m.lines().get(key))));
 	}
 
 	private static void callInBackground(CallExecutor callExecutor, Call call) {
@@ -79,7 +88,11 @@ class CallExecutorTest {
 	}
 
 	private static long count(List<MessageToSend> sent, String command) {
-		return sent.stream().map(MessageToSend::command).filter(isEqual(command)).count();
+		return sent.stream().filter(where(MessageToSend::command, isEqual(command))).count();
+	}
+
+	private static Predicate<MessageToSend> where(Function<MessageToSend, String> mapper, Predicate<String> predicate) {
+		return m -> predicate.test(mapper.apply(m));
 	}
 
 }
